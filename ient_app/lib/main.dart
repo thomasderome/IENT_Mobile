40116d21bf4ffb96325bc7b1684fc776cac6a45b
Thomas Derome <thomas.derome@epitech.eu>
2026-02-19 19:54:13 +0100

    [~] init api on main

--- ient_app/lib/main.dart
+++ ient_app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'login_page/login_page.dart';
+import '../backend/backend.dart' as back;
 
 void main() {
+  back.API();
   runApp(const Login_page());
 }
