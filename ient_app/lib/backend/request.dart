366d1365f93a594aa507ed793f5e2cb47cb1ed8e
Thomas Derome <thomas.derome@epitech.eu>
2026-02-19 19:53:47 +0100

    [~] update request system remove auto redirect for cookiejar and add new function for redirect system

--- ient_app/lib/backend/request.dart
+++ ient_app/lib/backend/request.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:cookie_jar/cookie_jar.dart';
+import 'package:flutter/material.dart';
 
 class HttpRequest {
   late Dio dio;
@@ -8,34 +9,38 @@ class HttpRequest {
 
   HttpRequest() {
     dio = Dio(BaseOptions(
-      connectTimeout: const Duration(seconds: 5),
-      receiveTimeout: const Duration(seconds: 3),
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 10),
       headers: {
         "User-Agent": "Mozilla/5.0 (Linux; Android 9; motorola one vision) AppleWebKit/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+      },
+      followRedirects: false,
+      validateStatus: (status) {
+        return status != null && status < 400;
       },
     ));
     dio.interceptors.add(CookieManager(cookieJar));
-
-    dio.interceptors.add(LogInterceptor(
-      requestHeader: true,  // Pour voir les cookies envoyés
-      responseHeader: true, // Pour voir les cookies reçus (Set-Cookie)
-      requestBody: true,
-      responseBody: false,
-    ));
   }
 
-  Future<Response> get(String url) async {
-    final response = await dio.get(url);
+  Future<Response> get(String url, Options? options) async {
+    Response response = await dio.get(url, options: options);
     return response;
   }
 
-  Future<Response> post(String url, Map<String, String> params) async {
-    final response = await dio.post(url, options: Options(
-        followRedirects: true,
-        contentType: "application/x-www-form-urlencoded",
-      ),
-      data: params,
-    );
+  Future<Response> post(String url, Map<String, String> params, Options? options) async {
+    Response response = await dio.post(url, options: options, data: params);
+    return response;
+  }
+
+  Future<Response> redirect_system(String url) async {
+    Response response = await dio.get(url);
+    String redirect = response.headers.value("location") ?? "";
+
+    while (redirect != "") {
+      response = await dio.get(redirect);
+      redirect = response.headers.value("location") ?? "";
+    }
     return response;
   }
 }
