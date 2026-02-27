df9d9d46104261d53a545184468bf542f99908c9
Thomas Derome <thomas.derome@epitech.eu>
2026-02-27 09:12:52 +0100

    [~] update verif login, [+] add request get_planning

--- ient_app/lib/backend/backend.dart
+++ ient_app/lib/backend/backend.dart
@@ -1,7 +1,8 @@
 import 'request.dart';
 import 'html_parser.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' as mat;
 import 'package:dio/dio.dart';
+import 'package:html/dom.dart';
 
 class API {
   // Single skeleton
@@ -33,21 +34,65 @@ class API {
     // Change url if add for account parent
     Response login = await request.post("https://auth.ient.fr/cas/login?service=https%3A%2F%2Fwww.ient.fr%2Flogin%3Fprofil%3D2", params, Options(contentType: "application/x-www-form-urlencoded"));
     String redirect_url = login.headers.value("location") ?? "";
-    Response verif = await request.redirect_system(redirect_url);
 
-    if (verif.realUri.toString() == "https://www.ient.fr/Welcome") return true;
-    return false;
+    if (redirect_url != "") {
+      Response verif = await request.redirect_system(redirect_url);
+      if (verif.realUri.toString() == "https://www.ient.fr/Welcome") return true;
+
+      return false;
+    }
+    else return false;
   }
 
-  Future<String> get_planning({String date = "", String link = ""}) async {
+  Future<List<Map>> get_planning({String date = ""}) async {
     // 2026-02-16
-    final login = await request.get(link, Options());
-    debugPrint(login.isRedirect.toString());
-    debugPrint(login.redirects.toString());
+    Response response = await request.get("https://www.ient.fr/planninghebdo?date=$date", Options());
+    Document planning_parse = await parser.html_parse(response.data);
 
-    final response = await request.get("https://www.ient.fr/planninghebdo?date=$date", Options());
-    final planning_parse = await parser.html_parse(response.data);
+    List<Element> days = planning_parse.getElementsByClassName("col-sm-1 col-12");
+    RegExp reg = RegExp(r"top:\s*([\d.]+)px;?\s*height:\s*([\d.]+)px");
 
-    return "test";
+    List<Map> result = [];
+
+    for (Element day in days) {
+      List<Map> temp = [];
+
+      String date = day.getElementsByClassName("entete-jour_planning")[0].text
+          .trim();
+      List<Element> sequences = day.getElementsByClassName("sequence");
+
+      for (Element sequence in sequences) {
+        if (sequence.id != "") {
+          String style = sequence.attributes["style"] ?? "";
+          Match? match = reg.firstMatch(style)!;
+
+          double start = int.parse(match.group(1)!) / 21;
+          double time = (int.parse(match.group(2)!) + 3) / 21;
+          
+          List<Element> data_activity = sequence.getElementsByClassName("txt_planning");
+
+          temp.add({
+            "name": data_activity[0].text.trim(),
+            "prof": data_activity[1].text.trim(),
+            "room": data_activity[2].text.trim(),
+            "time": time,
+            "start": start
+          });
+        } else {
+          temp.add({
+            "name": sequence.text.trim(),
+            "prof": "",
+            "room": "",
+            "time": 26,
+            "start": 0
+          });
+        }
+      }
+      result.add({
+        "day": date,
+        "activity": temp
+      });
+    }
+    return result;
   }
 }
