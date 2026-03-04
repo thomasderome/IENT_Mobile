b6f3878f1db16e285b57f7a95072be917a2684b0
Thomas Derome <thomas.derome@epitech.eu>
2026-03-04 13:06:58 +0100

    [-] remove package don't use

--- ient_app/lib/backend/request.dart
+++ ient_app/lib/backend/request.dart
@@ -1,7 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:cookie_jar/cookie_jar.dart';
-import 'package:flutter/material.dart';
 
 class HttpRequest {
   late Dio dio;
