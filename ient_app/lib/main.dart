e582c176b887ae41ec6a51a938ef595f8d4458bf
Thomas Derome <thomas.derome@epitech.eu>
2026-03-19 16:53:20 +0100

    [+] Add system verif already login and redirect to the good page

--- ient_app/lib/main.dart
+++ ient_app/lib/main.dart
@@ -2,9 +2,23 @@ import 'package:flutter/material.dart';
 import 'page/login_page.dart';
 import 'page/home_page.dart';
 import '../backend/backend.dart' as back;
+import 'package:flutter_keystore/flutter_keystore.dart';
 
-void main() {
-  back.API();
-  runApp(const Home_page());
-  //runApp(const Login_page());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final API = back.API();
+
+  final FlutterKeystore keystore = FlutterKeystore();
+
+  String? username = await keystore.read("username");
+  String? password = await keystore.read("password");
+
+  if (username != null && password != null) {
+    bool verif = await API.login(username, password);
+
+    if (verif) {runApp(const Home_page());}
+    else {runApp(const Login_page());}
+
+  } else {runApp(const Login_page());}
 }
