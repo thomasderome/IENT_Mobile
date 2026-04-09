7d9c0ea98eb8b3672bf665e3ec5bfbeb968707eb
Thomas Derome <thomas.derome@epitech.eu>
2026-04-09 15:06:14 +0200

    [~] Backend get work

--- ient_app/lib/backend/backend.dart
+++ ient_app/lib/backend/backend.dart
@@ -54,7 +54,7 @@ class API {
 
         final FlutterSecureStorage keystore = FlutterSecureStorage();
         await keystore.write(key: "account", value: jsonEncode(account));
-
+        await get_work();
         return true;
       }
     }
@@ -133,5 +133,30 @@ class API {
     };
 
     return result;
+  }
+
+  Future<Map<String, List>> get_work() async {
+    final Iterable<String>? work_days = home_page?.querySelectorAll("a[href^='/cahiertexte/travail?date=']").map((e) => e.attributes["href"]!).toSet();
+
+    Map<String, List> work = {};
+    for (final day in work_days!) {
+      final work_page = await request.get("https://www.ient.fr${day}", Options());
+      final work_html = await parser.html_parse(work_page.data);
+
+      final works_day = work_html.querySelectorAll('div[id^="modal_"]');
+
+      for (Element work_day in works_day) {
+        if (work[day] == null) work[day] = [];
+
+        work[day]?.add({
+          "title": work_day.getElementsByTagName("h3")[0].text,
+          "desc_simple": "",
+          "desc_detail": work_day.querySelector('div[class^="modal-body"]')?.querySelector("div[style^='padding-left:10px;padding-right:10px;']")?.text,
+          "doc": work_day.querySelectorAll('a[href]').map((e) => "https://www.ient.fr/${e.attributes['href']}"),
+        });
+      }
+    }
+
+    return work;
   }
 }
