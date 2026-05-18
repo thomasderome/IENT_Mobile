0dc9c0b197ddb75b1594743a5001d80223c7dfa9
thomasderome <thomas.derome@epitech.eu>
2026-05-18 13:10:09 +0200

    [~] Add version and rename app package

--- ient_app/android/app/build.gradle.kts
+++ ient_app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.ient_app"
+    namespace = "com.thomas_derome.ient_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,9 +21,13 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.ient_app"
+        applicationId = "com.thomas_derome.ient_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
+        targetSdkVersion 34
+        versionCode 1
+        versionName "1.0.1"
+
         minSdk = 28
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
